47abd9a910981c1fe6d363d14d6d74034a3e7d34
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-08 12:39:34 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
-
+class Book 
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+    def initialize(title)
+      @title = title
+      @page_count = page_count
+    end
+    def turn_page
+      puts 'Flipping the page...wow, you read fast!'
+    end
+end
 
